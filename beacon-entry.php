0a98aae16b3af7845037489e2ad5c5c9b48ffc5c
root <root@hulea.org>
2014-11-05 19:14:24 +0000

    These are the files needed for the beacon page template

---
+++ beacon-entry.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="author" content="Nick Hulea"/>
+    <meta name="description" content="Template OLE App Beacon Entry"/>
+    <title>Template OLE App</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/beacon_entry.css"/>
+  </head>
+  <body>
+    <section id="heroTop" class="fullWidth"> 
+      <img class="hero" src="img/global/10_eagle-copy.png" />
+    </section>
+    <section id="detailTopBeacon" class="inner">
+      <h1>nhm eagle</h1>
+      <p>unlocked: 1/12</p>
+    </section>
+    <hr class="shadowed" />
+    <section id="detailBottomBeacon" class="descriptionBeacon">
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+    </section>
+  </body>
+</html>
